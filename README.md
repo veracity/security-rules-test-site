df9598364660a42b1a39a89e88869112d0c7941a
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-11-05 16:48:02 +0100

    implementerd feature policy, updated readme

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ This is a simple site hosted on heroku that implements strict CSP and security r
 
 ## CSP
 CSP configuration should be as strict as possible, that is "self" on all properties. The site should have the best score possible on https://securityheaders.com/
+
+## Test site
+The test site is hosted here: https://security-rules-test-site.herokuapp.com/
+
+## Workflow
+The site is currently on my personal account (since it's free) - contact me at sigurd.wahl@dnvgl.com if you need help. Steps to test you stuff:
+1) Checkout a new branch from the master branch
+2) Make you changes and push the branch to remove
+3) Go here: https://dashboard.heroku.com/apps/security-rules-test-site and click "Deploy" tab
+4) Go to "Manual deploy" section
+5) Select your branch and hit the "Deploy" button
--- server.js
+++ server.js
@@ -1,20 +1,58 @@
 const express = require("express")
 const helmet = require("helmet")
+const featurePolicy = require("feature-policy")
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(helmet({
-	contentSecurityPolicy: {
-		
+app.use(helmet());
+app.use(featurePolicy({
+	features: {
+		accelerometer: ["'none'"],
+		ambientLightSensor: ["'none'"],
+		autoplay: ["'none'"],
+		battery: ["'none'"],
+		camera: ["'none'"],
+		displayCapture: ["'none'"],
+		documentDomain: ["'none'"],
+		documentWrite: ["'none'"],
+		encryptedMedia: ["'none'"],
+		executionWhileNotRendered: ["'none'"],
+		executionWhileOutOfViewport: ["'none'"],
+		fontDisplayLateSwap: ["'none'"],
+		fullscreen: ["'none'"],
+		geolocation: ["'none'"],
+		gyroscope: ["'none'"],
+		layoutAnimations: ["'none'"],
+		legacyImageFormats: ["'none'"],
+		loadingFrameDefaultEager: ["'none'"],
+		magnetometer: ["'none'"],
+		microphone: ["'none'"],
+		midi: ["'none'"],
+		navigationOverride: ["'none'"],
+		notifications: ["'none'"],
+		oversizedImages: ["'none'"],
+		payment: ["'none'"],
+		pictureInPicture: ["'none'"],
+		publickeyCredentials: ["'none'"],
+		push: ["'none'"],
+		serial: ["'none'"],
+		speaker: ["'none'"],
+		syncScript: ["'none'"],
+		syncXhr: ["'none'"],
+		unoptimizedImages: ["'none'"],
+		unoptimizedLosslessImages: ["'none'"],
+		unoptimizedLossyImages: ["'none'"],
+		unsizedMedia: ["'none'"],
+		usb: ["'none'"],
+		verticalScroll: ["'none'"],
+		vibrate: ["'none'"],
+		vr: ["'none'"],
+		wakeLock: ["'none'"],
+		xr: ["'none'"],
+		xrSpatialTracking: ["'none'"]
 	}
-}));
-
-app.use(
-	helmet({
-		referrerPolicy: { policy: "no-referrer" },
-	})
-);
+}))
 
 app.get("/", (req, res) => {
 	res.send("Hello World!")
